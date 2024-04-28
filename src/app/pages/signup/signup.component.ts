08022f82ea1a36409443b06c22821ea568fe79a6
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-28 15:08:20 +0300

    add todos styles

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  userData: FormGroup;
 
+  constructor() {
+    this.userData = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.userData.valid) {
+      console.log(this.userData.value);
+    } else {
+      console.log('Not Valid Data');
+    }
+  }
 }
